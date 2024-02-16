d694012bae1e72eba2ba72695ebda63c673bcef6
Mehebul02 <mehebul2123@gmail.com>
2024-02-16 10:53:05 +0600

    total complite

--- index.js
+++ index.js
@@ -2,16 +2,52 @@
 const allBtn=document.getElementsByClassName('add-btn')
 let count =0
 for(const btn of allBtn){
-   btn.addEventListener('click',function(){
+   btn.addEventListener('click',function halnDle (e){
     count +=1
     setInnerText('cart-count',count)
     const selectedContainer=document.getElementById('selected-place-container')
+    const li =document.createElement('li')
+    const p1=document.createElement("p")
+    const p2 =document.createElement('p')
+    li.appendChild(p1)
+    li.appendChild(p2)
+    selectedContainer.appendChild(li)
+    // Place Name 
+    const placeName=e.target.parentNode.childNodes[1].innerText
+     p1.innerText=placeName
+
+    //  Price 
+    const price =e.target.parentNode.childNodes[3].childNodes[1].innerText
+    p2.innerText=price
+
+    // total cost 
+    // let sum =0
+    // const totalcost=document.getElementById('total-cost')
+
+// totalPrice('total-cost',total)
+// let total=0;
+// const totalPrice=document.getElementById('total-cost').innerText
+// const totalcost=parseInt(total)+parseInt(totalPrice);
+// console.log(totalcost.innerText)
+
+totalPrice("total-cost",price)
+  
+  
+    
     
 
     
    })
 }
 
+
+// Total Price 
+function totalPrice(id,value){
+    const totalCost=document.getElementById(id).innerText;
+    const total =parseInt(totalCost)+parseInt(value)
+    document.getElementById(id).innerText=total
+
+}
 
 // Set InnerText 
 function setInnerText(id,value){
